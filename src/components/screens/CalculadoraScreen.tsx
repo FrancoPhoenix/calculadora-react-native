b814c1d0573a64624322946767ee7b642714f58e
Franco <franco.phoenix@gmail.com>
2021-05-30 15:28:28 -0500

    add basic layout

---
+++ src/components/screens/CalculadoraScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+
+export const CalculadoraScreen = () => {
+    return (
+        <View>
+            <Text>Calculadora</Text>
+        </View>
+    )
+}
